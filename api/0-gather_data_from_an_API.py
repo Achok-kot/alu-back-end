1f62428358602a57ab5d20709393fe4f505658b0
Achok-kot <p.kot@alustudent.com>
2026-02-22 00:45:07 +0200

    todo list

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,14 +6,16 @@ import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+
     user = requests.get(user_url).json()
     todos = requests.get(todos_url).json()
-    
+
     completed = [task for task in todos if task.get("completed")]
-    
-    print(f"Employee {user.get('name')} is done with tasks({len(completed)}/{len(todos)}):")
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
     for task in completed:
-        print(f"\t {task.get('title')}")
+        print("\t {}".format(task.get("title")))
